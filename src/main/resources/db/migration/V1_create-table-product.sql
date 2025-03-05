330b609439cad089ed9921713b185922da3bb015
TrocaTine <trocatine@gmail.com>
2025-03-05 20:38:50 -0300

    Arrumando a criação da table

--- src/main/resources/db/migration/V1_create-table-product.sql
+++ src/main/resources/db/migration/V1_create-table-product.sql
@@ -2,5 +2,7 @@ CREATE TABLE product
 (
     id             TEXT PRIMARY KEY UNIQUE NOT NULL,
     name           TEXT                    NOT NULL,
-    price_in_cents INT                  NOT NULL
+    price_in_cents INT                  NOT NULL,
+    description    TEXT                    NOT NULL,
+    image_url      TEXT                    NOT NULL
 );
