4155b9e6bd13f5bf7d0058a716bf54549ca61c5b
TrocaTine <trocatine@gmail.com>
2025-03-05 21:33:59 -0300

    Arrumando os getter e setters

--- src/main/java/com/study/APIproduct/domain/product/Product.java
+++ src/main/java/com/study/APIproduct/domain/product/Product.java
@@ -1,7 +1,10 @@
 package com.study.APIproduct.domain.product;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import lombok.Getter;
 
 @Getter
 @Setter
@@ -22,5 +25,45 @@ public class Product {
         this.price_in_cents = requestProduct.price_in_cents();
         this.description = requestProduct.description();
         this.image_url = requestProduct.image_url();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    public void setPrice_in_cents(int price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
     }
 }
