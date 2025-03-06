75a9049db71666d398606d1fcc3a38e5ee50ebee
Vinicius Abs Soares <vinicius.soares@germinare.org.br>
2025-03-06 11:33:48 -0300

    API funcionando SQL

--- src/main/java/com/study/APIproduct/domain/product/Product.java
+++ src/main/java/com/study/APIproduct/domain/product/Product.java
@@ -3,11 +3,9 @@ package com.study.APIproduct.domain.product;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
+
 @Table(name = "product")
 @Entity
-@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Product {
     @Id
@@ -16,11 +14,53 @@ public class Product {
     private String name;
     private int price_in_cents;
     private String description;
-    private String image_url;
+    private String url_image;
     public Product(RequestProduct requestProduct) {
         this.name = requestProduct.name();
         this.price_in_cents = requestProduct.price_in_cents();
         this.description = requestProduct.description();
-        this.image_url = requestProduct.image_url();
+        this.url_image = requestProduct.image_url();
+    }
+    public Product() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl_image() {
+        return url_image;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice_in_cents(int price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUrl_image(String image_url) {
+        this.url_image = image_url;
     }
 }
